3497255118ba57683f55f3d2e928fa983a18c254
Todueyungbo <101442241+Todueyungbo@users.noreply.github.com>
2024-08-10 01:51:30 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Import React 18's ReactDOM client
+import './index.css'; // Import your global CSS
+import App from './App';
+
+// Find the root element
+const rootElement = document.getElementById('root');
+
+// Create a root to render your app into
+const root = ReactDOM.createRoot(rootElement);
+
+// Render the App component into the DOM
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
